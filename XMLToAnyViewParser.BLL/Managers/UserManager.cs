b1a426c796f09fdf3534a83e513767ccc2e0c45c
lukics <lukic.sasa11@gmail.com>
2017-08-28 21:42:43 +0200

    login method added

---
+++ XMLToAnyViewParser.BLL/Managers/UserManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using XMLToAnyViewParser.Common.Exceptions;
+using XMLToAnyViewParser.Common.Helpers;
+using XMLToAnyViewParser.Data.UnitOfWork;
+using XMLToAnyViewParser.Entities;
+
+namespace XMLToAnyViewParser.BLL.Managers
+{
+    public class UserManager
+    {
+        public static User Login(string username, string password)
+        {
+            using (var uow = new UnitOfWork())
+            {
+                if (string.IsNullOrWhiteSpace(username)) throw new ValidationException("You must provide login data!");
+
+                var user = uow.UserRepository.Find(u => u.Username.ToLower().Trim() == username.ToLower().Trim() || u.Email.ToLower().Trim() == username.ToLower().Trim()).FirstOrDefault();
+                if (user == null || !PasswordHelper.ValidatePassword(password, user.Password)) throw new ValidationException("Wrong username or password!");
+                return user;
+            }
+        }
+    }
+}
---
+++ XMLToAnyViewParser.Service/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using XMLToAnyViewParser.BLL.Managers;
+using XMLToAnyViewParser.Common.Helpers;
+using XMLToAnyViewParser.Common.Models;
+using XMLToAnyViewParser.Entities;
+using XMLToAnyViewParser.Service.Models;
+
+namespace XMLToAnyViewParser.Service.Controllers
+{
+    public class UserController : Controller
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public object Login(LoginModel model)
+        {
+            User user = UserManager.Login(model.Email, model.Password);
+            UserModel userModel = Mapper.Map(user);
+            return new { User = userModel, Token = CreateLoginToken(user) };
+        }
+
+        [NonAction]
+        private string CreateLoginToken(User user)
+        {
+            UserJwtModel userModel = Mapper.AutoMap<User, UserJwtModel>(user);
+            userModel.ExpirationDate = DateTime.UtcNow.AddDays(1);
+
+            string secretKey = WebConfigurationManager.AppSettings["JwtSecret"];
+            string token = JWT.JsonWebToken.Encode(userModel, secretKey, JWT.JwtHashAlgorithm.HS256);
+            return token;
+        }
+    }
+}
