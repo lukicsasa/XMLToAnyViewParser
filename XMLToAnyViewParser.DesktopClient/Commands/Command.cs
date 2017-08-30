a2564c19af460fee898ed4556d3244e7d6f0b00d
lukics <lukic.sasa11@gmail.com>
2017-08-31 00:54:42 +0200

    premerge

---
+++ XMLToAnyViewParser.DesktopClient/Commands/Command.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XMLToAnyViewParser.DesktopClient.Commands
+{
+    public class Command : ICommand
+    {
+        #region Data members
+
+        private Predicate<object> canExecute;
+
+        private Action<object> execute;
+
+        #endregion
+
+        #region Constructors
+
+        public Command(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public Command(Action<object> execute)
+        {
+            this.execute = execute;
+            this.canExecute = null;
+        }
+
+        #endregion
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
---
+++ XMLToAnyViewParser.DesktopClient/Helpers/UrlBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLToAnyViewParser.DesktopClient.Helpers
+{
+    public static class UrlBuilder
+    {
+        private static readonly string URL = "http://localhost:52042/api/";
+
+        public static Uri GetUrl(string url)
+        {
+            return new Uri(URL + url);
+        }
+    }
+}
+
